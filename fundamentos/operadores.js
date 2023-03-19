dac8f541901c1045c3242f0787e2cdc165545546
Wagner Silva Souza <souzawagner.ti@gmail.com>
2023-03-18 21:15:52 -0300

    Operadores

---
+++ fundamentos/operadores.js
@@ -0,0 +1,37 @@
+/**
+ * Operadores:
+ * Aritiméticos
+ * Aritiméticos de atribuição
+ * Comparação
+ * Lógicos
+ */
+
+console.log("Operadores aritiméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("% | módulo (sobra de divisão")
+console.log("")
+console.log("Operadores aritiméticos de atribuição")
+console.log("+= | soma e atribuição a variável")
+console.log("-= | subtração e atribuição de variável")
+console.log("*= | multiplicação e atribuição de variável")
+console.log("/= | divisão e atribuição de variável")
+console.log("++ | soma 1 ao resultado")
+console.log("-- | subtrai 1 do resultado")
+console.log("")
+console.log("Operadores de comparação")
+console.log(">  | maior que")
+console.log("<  | menor que")
+console.log(">= | maior ou igual a")
+console.log("<= | menor ou igual a")
+console.log("== | igual a")
+console.log("===| identico a")
+console.log("!= | diferente de")
+console.log("!==| não identico a")
+console.log("")
+console.log("Operadores lógicos")
+console.log("&& | And")
+console.log("|| | Or")
+console.log("!  | Not")
